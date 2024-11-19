d329253dd475036624cebfe707180cfa60be1607
mrx <meibrohimov@gmail.com>
2024-11-19 12:29:47 +0500

    add new feature

--- bot/main.py
+++ bot/main.py
@@ -2,14 +2,15 @@ import asyncio
 from aiogram import Bot, Dispatcher
 from bot.config import BOT_TOKEN
 from bot.handlers import register_all_handlers
+from bot.middlewares.logging_middeware import CustomLoggingMiddleware
 
 
 async def main():
     bot = Bot(token=BOT_TOKEN)
     dp = Dispatcher()
+    dp.message.middleware(CustomLoggingMiddleware())
     register_all_handlers(dp)
     await dp.start_polling(bot)
-
 
 
 if __name__ == '__main__':
