6670ea4187a5dc1b9a5cf927b17f764c4d01e79b
mrx <meibrohimov@gmail.com>
2024-11-22 09:46:04 +0500

    add new feature requirements.txt

--- models/user.py
+++ models/user.py
@@ -13,6 +13,9 @@ class User(Model):
     referred_by = fields.ForeignKeyField("models.User", related_name="referrals", null=True)
     created_at = fields.DatetimeField(auto_now_add=True)
 
+    class Meta:
+        table = "users"
+
     @classmethod
     async def generate_unique_referral_code(cls):
         while True:
@@ -21,9 +24,18 @@ class User(Model):
                 return referral_code
 
 
+
+
 async def add_user(tg_id: int, full_name: str, username: str, referred_by_code: str = None):
     referral_code = await User.generate_unique_referral_code()
-    user = User(tg_id=tg_id, full_name=full_name, username=username, referral_code=referral_code, referred_by=None)
+
+    user = User(
+        tg_id=tg_id,
+        full_name=full_name,
+        username=username,
+        referral_code=referral_code,
+        referred_by=None
+    )
 
     if referred_by_code:
         referrer = await User.get_or_none(referral_code=referred_by_code)
